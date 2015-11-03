e2412962d25eeace7f46fe57e1965ad244b763b3
tescom <tescom@atdt01410.com>
2015-11-03 15:24:30 +0900

    add trigger

--- run.sh
+++ run.sh
@@ -2,3 +2,9 @@
 
 mv /etc/phpmyadmin/config-db.php /data/
 ln -s /data/config-db.php /etc/phpmyadmin/config-db.php
+
+. /etc/apache2/envvars
+exec dockerize \
+     -stdout /var/log/apache2/access.log \
+     -stderr /var/log/apache2/error.log \
+     /usr/sbin/apache2 -DFOREGROUND
