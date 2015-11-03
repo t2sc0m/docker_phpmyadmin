ad3eeb863c5b9efda777e62ca1122dc8e3002f40
tescom <tescom@atdt01410.com>
2015-11-03 17:18:03 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,6 @@
 
 mv /etc/phpmyadmin/config-db.php /data/
 ln -s /data/config-db.php /etc/phpmyadmin/config-db.php
-chown www-data:www-data /data/config-db.php
+chown www-data:www-data /data -R
 
-. /etc/apache2/envvars
-exec dockerize \
-     -stdout /var/log/apache2/access.log \
-     -stderr /var/log/apache2/error.log \
-     /usr/sbin/apache2 -DFOREGROUND
+. /apache-run.sh
