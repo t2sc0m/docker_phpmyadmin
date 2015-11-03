49d79d065895b7df7bb69d488094d5fd72d18a38
tescom <tescom@atdt01410.com>
2015-11-03 16:50:09 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 mv /etc/phpmyadmin/config-db.php /data/
 ln -s /data/config-db.php /etc/phpmyadmin/config-db.php
+chown www-data:www-data /data/config-db.php
 
 . /etc/apache2/envvars
 exec dockerize \
