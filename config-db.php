f5d95eec4b8172055e946ed212d76db220155a09
tescom <tescom@atdt01410.com>
2015-11-03 15:36:52 +0900

    update config-db.php

--- config-db.php
+++ config-db.php
@@ -1,11 +1,11 @@
 <?php
 $dbuser=getenv('DB_ENV_MYSQL_USER') ? :	 	 # https://registry.hub.docker.com/u/tutum/mysql/
-	getenv('DB_USER') ? :			 # manual 
-	'root';
+	    getenv('DB_USER') ? :			 # manual
+	    'root';
 $dbpass=getenv('DB_ENV_MYSQL_ROOT_PASSWORD') ? : # https://registry.hub.docker.com/_/mysql/
         getenv('DB_ENV_MYSQL_PASS') ? : 	 # https://registry.hub.docker.com/u/tutum/mysql/
-	getenv('DB_ENV_MARIADB_PASS') ? :        # https://registry.hub.docker.com/u/tutum/mariadb/
-	getenv('DB_PASSWORD');                   # manual
+	    getenv('DB_ENV_MARIADB_PASS') ? :        # https://registry.hub.docker.com/u/tutum/mariadb/
+	    getenv('DB_PASSWORD');                   # manual
 $basepath='';
 $dbname='phpmyadmin';
 if (preg_match('@^tcp://([\d.]+):(\d+)$@', getenv('DB_PORT'), $m)) {
